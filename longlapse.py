5e52b4e17be0e7ba7f04b0614d6807f5a433af9b
barlaensdoonn <brandon.aleson@gmail.com>
2016-12-08 01:09:39 -0800

    changed copying to one file at a time to ensure success and/or track failures; seems best to use rsync tho because this adds a lot of complexity to otherwise simple methods

--- longlapse.py
+++ longlapse.py
@@ -11,18 +11,20 @@ import datetime
 import os
 import shutil
 import subprocess
+# import shlex
 import logging
 import traceback
 from fractions import Fraction
-from longpaths import base_remote_path
+from longpaths import remote_path, host
 
 log_locate = '/home/pi/longlapse/longlapse.log'
+
 
 class Camera(object):
 
     def __init__(self):
         self.base_pi_path = '/home/pi/longlapse'
-        self.base_remote_path = base_remote_path
+        self.remote_path = remote_path
         self.copied = False
         self.pixels = (2592, 1944)
         self.framerate = 1
@@ -73,22 +75,59 @@ class Camera(object):
         if not os.path.isdir(self.todays_dir):
             os.mkdir(self.todays_dir)
 
+    def make_remote_dir(self, today):
+        self.remote_dir = os.path.join(self.remote_path, today)
+        status = subprocess.call(['ssh', host, 'test -d {}'.format(self.remote_dir)], stdout=subprocess.DEVNULL, stderr=log_locate)
+
+        if status == 1:
+            remote = subprocess.call(['ssh', host, 'mkdir {}'.format(self.remote_dir)])
+
+        if status == 1 and remote == 0:
+            logging.info('created remote directory at {}'.format(self.remote_dir))
+            return True
+        elif status == 0:
+            logging.info('remote directory already exists at {}'.format(self.remote_dir))
+            return True
+        else:
+            logging.warning('problem creating remote directory at {}'.format(self.remote_dir))
+            return False
+
     def copy_todays_dir(self, today):
-        file_path = 'Desktop/picamera/first_run'
-        copy_from = os.path.join(self.base_pi_path, today)
-        copy_to = os.path.join(self.base_remote_path, file_path, today)
-        subprocess.call(['scp', '-rp', copy_from, copy_to], stdout=subprocess.DEVNULL)
-        self.copied = True
+        '''
+        to check if file exists on remote use:
+        subprocess.call(['ssh', host, 'test -f {}'.format(shlex.quote(path))])
+        '''
+        remote = self.make_remote_dir(today)
+
+        if remote:
+            status_dict = {}
+            pic_list = [thing for thing in os.listdir(self.todays_dir) if not thing.startswith('.')]
+            pic_list.sort()
+
+            for pic in pic_list:
+                pic_path = os.path.join(self.todays_dir, pic)
+                status = subprocess.call(['scp', '-p', pic_path, self.remote_dir], stdout=subprocess.DEVNULL, stderr=log_locate)
+                status_dict[pic] = status
+
+        for key in status_dict.keys():
+            if status_dict[key] == 1:
+                logging.warning("trouble copying {}".format(key))
+                self.copied = False
+        else:
+            self.copied = True
 
     def copy_log(self):
         dropbox_path = 'dropbox_me/Dropbox/longlapse'
         copy_log_from = os.path.join(self.base_pi_path, 'longlapse.log')
         copy_log_to = os.path.join(self.base_remote_path, dropbox_path)
-        subprocess.call(['scp', '-rp', copy_log_from, copy_log_to], stdout=subprocess.DEVNULL)
+        subprocess.call(['scp', '-p', copy_log_from, copy_log_to], stdout=subprocess.DEVNULL, stderr=log_locate)
 
     def delete_todays_dir(self):
         if os.path.isdir(self.todays_dir) and self.copied:
             shutil.rmtree(self.todays_dir)
+            logging.info("deleted today's directory")
+        else:
+            logging.warning("did not delete today's directory")
 
 
 class Light(object):
@@ -111,7 +150,7 @@ class Light(object):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename=log.locate, format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S', level=logging.INFO)
+    logging.basicConfig(filename=log_locate, format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S', level=logging.INFO)
 
     camera = Camera()
     light = Light()
@@ -138,13 +177,12 @@ if __name__ == '__main__':
 
         camera.take_pic(light.today)
 
-        # TODO: copy images one at a time and check exit status; try again if failed
+        # TODO: use rsync instead of these methods for file copying and directory deleting
         logging.info("copying today's directory to kestrel")
         camera.copy_todays_dir(light.today)
         logging.info("finished copying today's directory")
 
-        # camera.delete_todays_dir()
-        # logging.info("deleted today's directory")
+        camera.delete_todays_dir()
 
         logging.info("copying today's log to DropBox\n")
         camera.copy_log()
